1dc8237229dd3bf5b05db4e42981438638df0efd
SotaKojima <sotakojima0525@gmail.com>
2022-05-30 20:54:55 +0900

    サイズをremへ変更

--- src/components/WorksTitleMyworks.vue
+++ src/components/WorksTitleMyworks.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="title">
     <h1 class="works_title">MyWorks</h1>
-    <div class="Works_title_explain_layout">
-      <p class="Works_title_explain_sentence">A collection of work, past and present.Some built for today, for tomorrow and others.
+    <div class="works_title_explain_layout">
+      <p class="works_title_explain_sentence">A collection of work, past and present.Some built for today, for tomorrow and others.
       <br>From editional and web, to product and vision work.</p>
     </div>
   </div>
@@ -12,21 +12,24 @@
 $background:#F4F2F0;
 .title{
   background-color: $background;
-  padding-top: 50px;
-  padding-bottom: 83px;
+  padding-top: 5rem;
+  padding-bottom: 8.3rem;
   .works_title{
     text-align: center;
-    font: normal normal bold 260px/340px Ysans Std;
-    letter-spacing: 7.8px;
+    font: normal normal bold 26rem/34rem Ysans Std;
+    letter-spacing: 0.78rem;
     color: #000000;
   }
-  .Works_title_explain_layout{
-    padding-top: 45px;
-  .Works_title_explain_sentence{
+  .works_title_explain_layout{
+    padding-top: 5rem;
+  .works_title_explain_sentence{
     text-align: center;
-    font: normal normal medium 14.5px/21px Montserrat;
-    line-height: 1.4;
-    letter-spacing: 0.9px;
+    font-family: montserrat, sans-serif;
+    font-size: 1.5rem;
+    font-weight: 500;
+    font-style: normal;
+    letter-spacing: 0.07rem;
+    line-height: 1.6;
     color: #4E4E4E;
     }
   }
