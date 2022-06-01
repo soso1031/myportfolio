334f63c871a2abf2fe24c030e818b8f32506813d
SotaKojima <sotakojima0525@gmail.com>
2022-06-01 17:58:51 +0900

    戻るボタンのアニメーションの実装＆完成

--- src/components/BackButton.vue
+++ src/components/BackButton.vue
@@ -1,20 +1,43 @@
 <template>
-
-    <button class="close_button is-hover" aria-label="戻る">
+<div class=layout>
+    <button class="close_button layout_right" aria-label="戻る">
         <span></span>
         <span></span>
     </button>
+</div>
+
 
 </template>
 
 <style scoped lang="scss">
+
+.layout{
+  padding-top: 4.8rem;
+  padding-right: 3.7rem;
+  padding-bottom: 1rem;
+  display: flex;
+  justify-content: end;
+  background-color: #F4F2F0;
 .close_button{
+  padding: 0.1rem;
+  display: block;
   width: 17.8rem;
   height: 4.74rem;
   position: relative;
   border: none;
   background-color: transparent;
   cursor: pointer;
+  &:hover span:nth-of-type(1)::before{
+    animation-name: LineOutIN;
+    animation-duration: 0.5s;
+    animation-timing-function:ease-in-out;
+  }
+  &:hover span:nth-of-type(2)::before{
+    animation-name: LineOutIN;
+    animation-duration: 0.5s;
+    animation-delay: 0.2s;
+    animation-timing-function:ease-in-out;
+  }
 }
 
 .close_button  span:nth-of-type(1) {
@@ -26,8 +49,16 @@
     transform: translate(-50%,-50%) rotate(15deg);
     top: 50%;
     left: 50%;
-    background-color: #313131;
-    transition: 0.2s;
+    &::before{
+      content: '';
+      display: block;
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      background-color: #313131;
+    }
   }
 .close_button  span:nth-of-type(2) {
     content: '';
@@ -38,16 +69,27 @@
     transform: translate(-50%,-50%) rotate(-15deg);
     top: 50%;
     left: 50%;
-    background-color: #313131;
-    transition: 0.2s;
+    &::before{
+      content: '';
+      display: block;
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      background-color: #313131;
+    }
   }
+}
+
+
 
   // アニメーション情報
 
-  @keyframes line-outin{
+  @keyframes LineOutIN{
     0%{-webkit-transform:scale3d(1,1,1);transform:scale3d(1,1,1);-webkit-transform-origin:100% 0;transform-origin:100% 0}
     50%{-webkit-transform:scale3d(0,1,1);transform:scale3d(0,1,1);-webkit-transform-origin:100% 0;transform-origin:100% 0}
-    50.00001%{-webkit-transform:scale3d(0,1,1);transform:scale3d(0,1,1);-webkit-transform-origin:0 0;transform-origin:0 0}
+    51%{-webkit-transform:scale3d(0,1,1);transform:scale3d(0,1,1);-webkit-transform-origin:0 0;transform-origin:0 0}
     100%{-webkit-transform:scale3d(1,1,1);transform:scale3d(1,1,1);-webkit-transform-origin:0 0;transform-origin:0 0}
 }
 </style>
