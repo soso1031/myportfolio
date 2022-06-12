f1c4c96b583ecfea5e462389c47b42364a9125e2
SotaKojima <sotakojima0525@gmail.com>
2022-06-12 10:57:34 +0900

    scroll behavior対応

--- src/router.js
+++ src/router.js
@@ -1,41 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import WorkAll from './views/WorkAll.vue'
 import Oneroute from './views/Oneroute.vue'
-import Test from './views/Test.vue'
 
-// export default createRouter({
-//   history: createWebHistory(),
-//   routes: [
-//     {path: "/" , component: WorkAll},
-//     {path: "/works/oneroute" , component: Oneroute},
-//     {path: "/test" , component: Test}
-//   ],
-//   scrollBehavior (to, from, savedPosition) {
-//     if (savedPosition) {
-//       return savedPosition
-//     } else {
-//       return { x: 0, y: 0 }
-//     }
-//   }
-// })
+
+
 
 const routes = [
-    {path: "/" , component: WorkAll},
-    {path: "/works/oneroute" , component: Oneroute},
-    {path: "/test" , component: Test}
+    {path: "/" , name:"Work", component: WorkAll},
+    {path: "/works/oneroute" , name:"Oneroute", component: Oneroute},
 ]
-const scrollBehavior = (to, from, savedPosition) => {
-  if (savedPosition) {
-    return savedPosition;
-  } else {
-    return { top: 0 }
-  }
-};
+
+// const scrollBehavior = (to, from, savedPosition) => {
+//   if (savedPosition) {
+//     return savedPosition;
+//   } else {
+//     return { top: 0 }
+//   }
+// };
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-  scrollBehavior
+  scrollBehavior(to, from, savedPosition) {
+    return savedPosition || new Promise ((resolve)=>{
+      setTimeout(()=> resolve({top:0}),300)
+    })
+  }
 })
 
 export default router
